eaa9fcf7e48c780df2ca5e0d851fc4a44085adff
sealferov88 <xampri97@gmail.com>
2018-06-13 00:18:51 +0400

    Добавьте файлы проекта.

---
+++ Shapes/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Triangle
+    {
+        public String IdentifyTriagleType(double a, double b, double c)
+        {
+            if ((a + b) > c &&
+               (a + c) > b &&
+               (b + c) > a)
+            {
+                double maxSide;
+                double secondSide;
+                double thirdSide;
+
+                if (a >= b && a >= c)
+                {
+                    maxSide = a;
+                    secondSide = b;
+                    thirdSide = c;
+                }
+                else
+                {
+                    if (b >= a && b >= c)
+                    {
+                        maxSide = b;
+                        secondSide = a;
+                        thirdSide = c;
+                    }
+                    else
+                    {
+                        maxSide = c;
+                        secondSide = a;
+                        thirdSide = b;
+                    }
+                }
+
+                if (Math.Pow(maxSide, 2) == Math.Pow(secondSide, 2) + Math.Pow(thirdSide, 2)) 
+                {
+                    return "right";
+                }
+                else
+                {
+                    if (Math.Pow(maxSide, 2) < Math.Pow(secondSide, 2) + Math.Pow(thirdSide, 2)) 
+                    {
+                        return "acute";
+                    }
+                    else
+                    {
+                        return "obtuse";
+                    }
+                }
+            }
+            else
+            {
+                return "error";
+            }
+        }
+    }
+}
---
+++ UnitTest/TriangleTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void AcuteTest()
+        {
+            Shapes.Triangle triangle = new Shapes.Triangle();
+            string result = triangle.IdentifyTriagleType(10, 10, 10);
+            Assert.AreEqual("acute", result);
+        }
+
+        [TestMethod]
+        public void ErrorTest()
+        {
+            Shapes.Triangle triangle = new Shapes.Triangle();
+            string result = triangle.IdentifyTriagleType(1000, 10, 10);
+            Assert.AreEqual("error", result);
+        }
+
+        [TestMethod]
+        public void RightTest()
+        {
+            Shapes.Triangle triangle = new Shapes.Triangle();
+            string result = triangle.IdentifyTriagleType(4, 3, 5);
+            Assert.AreEqual("right", result);
+        }
+
+        [TestMethod]
+        public void ObtuseTest()
+        {
+            Shapes.Triangle triangle = new Shapes.Triangle();
+            string result = triangle.IdentifyTriagleType(6, 6, 10);
+            Assert.AreEqual("obtuse", result);
+        }
+    }
+}
